70e04cd320f1dce5da11a7d405e6852b71704314
Chiranjivee <chiranjivee960@gmail.com>
2021-09-26 11:38:33 +0530

    fix bugrq2

--- server.js
+++ server.js
@@ -10,17 +10,10 @@ app.use('/api', async (req, res) => {
   const val = getSecondMaximum(Arr);
   return res.send({ secondMax: val });
 });
-
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/client/build')));
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
-  });
-} else {
-  app.get('*', (req, res) => {
-    res.send('HURRAH');
-  });
-}
+app.use(express.static(path.join(__dirname, '/client/build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
